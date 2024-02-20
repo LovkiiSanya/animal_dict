5dbc491969d0e9f1a329eaa601c1dabaab1917cc
Sanya <160408798+LovkiiSanya@users.noreply.github.com>
2024-02-20 19:26:18 +0200

    Update onemoretime.py

--- onemoretime.py
+++ onemoretime.py
@@ -13,17 +13,122 @@ class HomeAnimal(Animal):  # дочка с особенностью клички
         super().__init__(id, color, place)
         self.name = name
 
+    def askinfo():
+        animalID = rndnumb()
+        animalcolor = input("Введите цвет животного: ")
+        animalplace = input("Введите место обитания животного: ")
+        animalname = input("Введите кличку животного: ")
+        typehomeanimal = HomeAnimal(animalID, animalcolor, animalplace, animalname)
+        animal_dict[animalID] = typehomeanimal
+        return print(
+            "Ваш ID:",
+            typehomeanimal.id,
+            typehomeanimal.color,
+            typehomeanimal.place,
+            typehomeanimal.name,
+        )
+
+    def changeinfo(animalID):
+        if type(animal_dict[changeanimal]) == HomeAnimal:
+            animalID = changeanimal
+            animalcolor = input("Введите цвет животного: ")
+            animalplace = input("Введите место обитания животного: ")
+            animalname = input("Введите кличку животного: ")
+            typehomeanimal = HomeAnimal(
+                changeanimal, animalcolor, animalplace, animalname
+            )
+            animal_dict[changeanimal] = typehomeanimal
+
+    def __repr__(self):
+        HomeAnimal = type(self).__name__
+        return f"{HomeAnimal}({self.id!r},{self.color!r},{self.place!r},{self.name!r})"
+
+    def __str__(self):
+        return f"Ваш ID: {self.id} Цвет: {self.color} Место обитания: {self.place} Кличка: {self.name}"
+
 
 class FriendlyAnimal(Animal):  # дочка с особенностью хвоста
     def __init__(self, id, color, place, tail):
         super().__init__(id, color, place)
         self.tail = tail
 
+    def askinfo():
+        animalID = rndnumb()
+        animalcolor = input("Введите цвет животного: ")
+        animalplace = input("Введите место обитания животного: ")
+        animaltail = input("Введите длинну хвоста в см: ")
+        typefriendlyanimal = FriendlyAnimal(
+            animalID, animalcolor, animalplace, animaltail
+        )
+        animal_dict[animalID] = typefriendlyanimal
+        return print(
+            "Ваш ID:",
+            typefriendlyanimal.id,
+            typefriendlyanimal.color,
+            typefriendlyanimal.place,
+            typefriendlyanimal.tail,
+        )
+
+    def changeinfo(animalID):
+        if type(animal_dict[changeanimal]) == FriendlyAnimal:
+            animalID = changeanimal
+            animalcolor = input("Введите цвет животного: ")
+            animalplace = input("Введите место обитания животного: ")
+            animaltail = input("Введите длинну хвоста в см: ")
+            typefriendlyanimal = FriendlyAnimal(
+                changeanimal, animalcolor, animalplace, animaltail
+            )
+            animal_dict[changeanimal] = typefriendlyanimal
+
+    def __repr__(self):
+        FriendlyAnimal = type(self).__name__
+        return (
+            f"{FriendlyAnimal}({self.id!r},{self.color!r},{self.place!r},{self.tail!r})"
+        )
+
+    def __str__(self):
+        return f"Ваш ID: {self.id} Цвет: {self.color} Место обитания: {self.place} Длинна хвоста: {self.tail}"
+
 
 class AgressiveAnimal(Animal):  # дочка с особенностью клыка
     def __init__(self, id, color, place, fang):
         super().__init__(id, color, place)
         self.fang = fang
+
+    def askinfo():
+        animalID = rndnumb()
+        animalcolor = input("Введите цвет животного: ")
+        animalplace = input("Введите место обитания животного: ")
+        animalfang = input("Введите длинну клыков в см: ")
+        typeagressiveanimal = AgressiveAnimal(
+            animalID, animalcolor, animalplace, animalfang
+        )
+        animal_dict[animalID] = typeagressiveanimal
+        return print(
+            "Ваш ID:",
+            typeagressiveanimal.id,
+            typeagressiveanimal.color,
+            typeagressiveanimal.place,
+            typeagressiveanimal.fang,
+        )
+
+    def changeinfo(animalID):
+        if type(animal_dict[changeanimal]) == AgressiveAnimal:
+            animalID = changeanimal
+            animalcolor = input("Введите цвет животного: ")
+            animalplace = input("Введите место обитания животного: ")
+            animalfang = input("Введите длинну клыков в см: ")
+            typeagressiveanimal = AgressiveAnimal(
+                changeanimal, animalcolor, animalplace, animalfang
+            )
+            animal_dict[changeanimal] = typeagressiveanimal
+
+    def __repr__(self):
+        AgressiveAnimal = type(self).__name__
+        return f"{AgressiveAnimal}({self.id!r},{self.color!r},{self.place!r},{self.fang!r})"
+
+    def __str__(self):
+        return f"Ваш ID: {self.id} Цвет: {self.color} Место обитания: {self.place} Длинна клыков: {self.fang}"
 
 
 def rndnumb():
@@ -60,55 +165,11 @@ while True:
             )
         )
         if addAnimal == 1:
-            animalID = rndnumb()
-            animalcolor = input("Введите цвет животного: ")
-            animalplace = input("Введите место обитания животного: ")
-            animalname = input("Введите кличку животного: ")
-            typehomeanimal = HomeAnimal(animalID, animalcolor, animalplace, animalname)
-            animal_dict[animalID] = typehomeanimal
-            print(animal_dict[animalID])
-            print(
-                "Ваш ID:",
-                typehomeanimal.id,
-                typehomeanimal.color,
-                typehomeanimal.place,
-                typehomeanimal.name,
-            )
+            HomeAnimal.askinfo()
         if addAnimal == 2:
-            animalID = rndnumb()
-            animalcolor = input("Введите цвет животного: ")
-            animalplace = input("Введите место обитания животного: ")
-            animaltail = input("Введите длинну хвоста в см: ")
-            typefriendlyanimal = FriendlyAnimal(
-                animalID, animalcolor, animalplace, animaltail
-            )
-            animal_dict[animalID] = typefriendlyanimal
-            print(animal_dict[animalID])
-            print(
-                "Ваш ID:",
-                typefriendlyanimal.id,
-                typefriendlyanimal.color,
-                typefriendlyanimal.place,
-                typefriendlyanimal.tail,
-            )
-
+            FriendlyAnimal.askinfo()
         if addAnimal == 3:
-            animalID = rndnumb()
-            animalcolor = input("Введите цвет животного: ")
-            animalplace = input("Введите место обитания животного: ")
-            animalfang = input("Введите длинну клыков в см: ")
-            typeagressiveanimal = AgressiveAnimal(
-                animalID, animalcolor, animalplace, animalfang
-            )
-            animal_dict[animalID] = typeagressiveanimal
-            print(animal_dict[animalID])
-            print(
-                "Ваш ID:",
-                typeagressiveanimal.id,
-                typeagressiveanimal.color,
-                typeagressiveanimal.place,
-                typeagressiveanimal.fang,
-            )
+            AgressiveAnimal.askinfo()
 
     if kindofaction == 3:
         infodel = int(input("Отлично,введите номер ID который удаляем: "))
@@ -117,32 +178,6 @@ while True:
 
     if kindofaction == 4:
         changeanimal = int(input("По какому ID будем вносить изменения?: "))
-        if type(animal_dict[changeanimal]) == HomeAnimal:
-            animalID = changeanimal
-            animalcolor = input("Введите цвет животного: ")
-            animalplace = input("Введите место обитания животного: ")
-            animalname = input("Введите кличку животного: ")
-            typehomeanimal = HomeAnimal(
-                changeanimal, animalcolor, animalplace, animalname
-            )
-            animal_dict[changeanimal] = typehomeanimal
-
-        if type(animal_dict[changeanimal]) == FriendlyAnimal:
-            animalID = changeanimal
-            animalcolor = input("Введите цвет животного: ")
-            animalplace = input("Введите место обитания животного: ")
-            animaltail = input("Введите длинну хвоста в см: ")
-            typefriendlyanimal = FriendlyAnimal(
-                changeanimal, animalcolor, animalplace, animaltail
-            )
-            animal_dict[changeanimal] = typefriendlyanimal
-
-        if type(animal_dict[changeanimal]) == AgressiveAnimal:
-            animalID = changeanimal
-            animalcolor = input("Введите цвет животного: ")
-            animalplace = input("Введите место обитания животного: ")
-            animalfang = input("Введите длинну клыков в см: ")
-            typeagressiveanimal = AgressiveAnimal(
-                changeanimal, animalcolor, animalplace, animalfang
-            )
-            animal_dict[changeanimal] = typeagressiveanimal
+        HomeAnimal.changeinfo(changeanimal)
+        FriendlyAnimal.changeinfo(changeanimal)
+        AgressiveAnimal.changeinfo(changeanimal)
